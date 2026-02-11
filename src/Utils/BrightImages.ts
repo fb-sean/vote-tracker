8895b664f5e8f5888b8339071e4082b365e2b7da
Sean Sattler <sean@sattler.dev>
2026-02-11 19:15:10 +0100

    [FEATURE] *added `BrightImages` enum with predefined image URLs

---
+++ src/Utils/BrightImages.ts
@@ -0,0 +1,8 @@
+export enum BrightImages{
+    Webhook = 'https://media.discordapp.net/attachments/843258152802844703/1471207358607917200/bright-webhook.png',
+    Clock = 'https://media.discordapp.net/attachments/843258152802844703/1471207358997729421/bright-clock.png',
+    Close = 'https://media.discordapp.net/attachments/843258152802844703/1471207359366959125/bright-close.png',
+    ThumbsUp = 'https://media.discordapp.net/attachments/843258152802844703/1471207359719407667/bright-done.png',
+    Shrug = 'https://media.discordapp.net/attachments/843258152802844703/1471207360080122128/bright-sad.png',
+    Peace = 'https://media.discordapp.net/attachments/843258152802844703/1471207360650543227/bright-smile.png'
+}
